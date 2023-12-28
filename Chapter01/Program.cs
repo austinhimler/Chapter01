94893b76bbf6d4e1f5a830a56bd7e0806fea874a
austinhimler <usclaxx10@gmail.com>
2023-12-28 12:13:26 -0500

    get rid of warning

--- Chapter01/Program.cs
+++ Chapter01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace CodeAnalyzing;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Debug.Write("Hello Debugger");
+    }
+}
